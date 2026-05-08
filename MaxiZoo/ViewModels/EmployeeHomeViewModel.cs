b7fa2827f1de2374bc463a4d4db9c9df23aff055
AsiyeDikenn <asiyediken@icloud.com>
2026-05-09 01:36:33 +0200

    Lavet tilbageknap på managerview og employeeview

--- MaxiZoo/ViewModels/EmployeeHomeViewModel.cs
+++ MaxiZoo/ViewModels/EmployeeHomeViewModel.cs
@@ -12,11 +12,15 @@ namespace MaxiZoo.ViewModels
     public class EmployeeHomeViewModel : BaseViewModel
     {
         private readonly NavigationStore _navigationStore;
+        private readonly NavigationService _navigationService;
         private readonly TaskService _taskService;
         private readonly CurrentUserStore _currentUserStore;
+        private readonly EmployeeRepository _employeeRepository;
+        private readonly EmployeeService _employeeService;
 
         public ICommand NavigateToMyTasksCommand { get; }
         public ICommand NavigateToTaskOverviewCommand { get; }
+        public ICommand NavigateBackCommand { get; }
 
         public EmployeeHomeViewModel(
             NavigationStore navigationStore,
@@ -27,8 +31,11 @@ namespace MaxiZoo.ViewModels
             EmployeeService employeeService)
         {
             _navigationStore = navigationStore;
+            _navigationService = navigationService;
             _taskService = taskService;
             _currentUserStore = currentUserStore;
+            _employeeRepository = employeeRepository;
+            _employeeService = employeeService;
 
 
             NavigateToMyTasksCommand = new NavigateCommand(
@@ -53,6 +60,17 @@ namespace MaxiZoo.ViewModels
                       currentUserStore,
                       employeeRepository,
                       employeeService)));
+
+            NavigateBackCommand = new NavigateCommand(
+                _navigationService,
+                () => new StartViewModel(
+                new UserIdentificationService(_employeeRepository),
+                      _currentUserStore,
+                      _navigationStore,
+                      _taskService,
+                      _employeeRepository,
+                      _employeeService,
+                      _navigationService));
         }
     }
 }
